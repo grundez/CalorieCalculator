02c5bdcd7524aeb48086f8b209efb88fdb1635b3
Maksim <mshaydurov7@gmail.com>
2025-04-29 03:04:57 +0700

    Класс CalorieCalculator добавлен для прохождения теста на его создание

--- CalorieCalculator/caloriecalculator.cpp
+++ CalorieCalculator/caloriecalculator.cpp
@@ -1,1 +1,6 @@
 #include "caloriecalculator.h"
+
+CalorieCalculator::CalorieCalculator()
+{
+
+}
--- CalorieCalculator/caloriecalculator.h
+++ CalorieCalculator/caloriecalculator.h
@@ -1,5 +1,10 @@
 #ifndef CALORIECALCULATOR_H
 #define CALORIECALCULATOR_H
 
+class CalorieCalculator
+{
+public:
+    CalorieCalculator();
+};
 
 #endif // CALORIECALCULATOR_H
